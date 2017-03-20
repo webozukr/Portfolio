6fdf749e844e34fba459725099c2862fbcae3597
webozukr <zoliv@ukr.net>
2017-03-20 23:08:01 +0200

    new files

--- scripts/script_1.js
+++ scripts/script_1.js
@@ -1,35 +1,32 @@
+$(document).ready(function () {
+    //step number
+    var slide_num = 2;
 
-                $(document).ready(function() {
-                    //step number
-                    var slide_num = 2;
+    //default view
+    $(".user-block-" + slide_num).toggleClass("hidden-block");
+    $(".circle-" + slide_num).toggleClass("small-circle-active");
 
-                    //default view
-                    $(".user-block-" + slide_num).toggleClass("hidden-block");
-                    $(".circle-" + slide_num).toggleClass("small-circle-active");
+    //click right
+    $(".slider-section-right").click(function () {
+        $(".user-block-" + slide_num).toggleClass("hidden-block");
+        $(".circle-" + slide_num).toggleClass("small-circle-active");
+        slide_num = slide_num + 1;
+        if (slide_num >= 4) {
+            slide_num = 3;
+        };
+        $(".user-block-" + slide_num).toggleClass("hidden-block");
+        $(".circle-" + slide_num).toggleClass("small-circle-active");
+    });
 
-                    //click right
-                    $(".slider-section-right").click(function() {
-                        $(".user-block-" + slide_num).toggleClass("hidden-block");
-                        $(".circle-" + slide_num).toggleClass("small-circle-active");
-                        slide_num = slide_num + 1;
-                        if (slide_num >= 4) {
-                            slide_num = 3;
-                        };
-                        $(".user-block-" + slide_num).toggleClass("hidden-block");
-                        $(".circle-" + slide_num).toggleClass("small-circle-active");
-                    });
-
-                    //click left
-                    $(".slider-section-left").click(function() {
-                        $(".user-block-" + slide_num).toggleClass("hidden-block");
-                        $(".circle-" + slide_num).toggleClass("small-circle-active");
-                        slide_num = slide_num - 1;
-                        if (slide_num <= 0) {
-                            slide_num = 1;
-                        };
-                        $(".user-block-" + slide_num).toggleClass("hidden-block");
-                        $(".circle-" + slide_num).toggleClass("small-circle-active");
-                    });
-                });
-
-            
+    //click left
+    $(".slider-section-left").click(function () {
+        $(".user-block-" + slide_num).toggleClass("hidden-block");
+        $(".circle-" + slide_num).toggleClass("small-circle-active");
+        slide_num = slide_num - 1;
+        if (slide_num <= 0) {
+            slide_num = 1;
+        };
+        $(".user-block-" + slide_num).toggleClass("hidden-block");
+        $(".circle-" + slide_num).toggleClass("small-circle-active");
+    });
+});
