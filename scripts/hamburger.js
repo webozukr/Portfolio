1a17c6bf447a330e53c9b6ee4c63a65a4aaab956
webozukr <zoliv@ukr.net>
2017-03-31 16:57:03 +0300

    some changes

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,5 +1,12 @@
-$(function () {
-    $('._hamburger').click(function () {
-        $('._nav').slideToggle();
+$(document).ready(function () {
+    $("._hamburger").click(function () {
+        $("._nav").slideToggle();
     });
-}); 
+    setInterval(function () {
+        if (window.matchMedia('(min-width: 825px)').matches) {
+            if ($('._nav:hidden')) {
+                $('._nav').slideDown();
+            };
+        };
+    }, 3000);
+});
